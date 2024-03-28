508c69ed734197867ceaa8c3f0f98d169f180961
Leonardo Drigo <leonardodrigo@MAC-0506.local>
2024-03-28 18:45:51 -0300

    removing tipo from pagamento + mock mysql

--- monolith/models.py
+++ monolith/models.py
@@ -78,7 +78,6 @@ class Pagamento(models.Model):
 
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     pago_em = models.DateTimeField(auto_now_add=True)
-    tipo = models.CharField(max_length=1, choices=TIPO_PAGAMENTO)
     veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
     status = models.CharField(max_length=1, choices=STATUS_PAGAMENTO)
     metodo_pagamento = models.CharField(max_length=1, choices=TIPO_PAGAMENTO, default='P')
